72acb071eb8bf5b1b2b1cfa79b26a0322300fc1a
Карина Ундалова <undalova2012@mail.ru>
2023-06-23 23:32:35 +0300

    23.06

--- deque.py
+++ deque.py
@@ -1,86 +1,79 @@
-#88460578
+#88494890
 
-ERROR_MESSAGE = 'error'
-ERROR_COMMAND = 'Error in command: {command}'
+class NoItemsException(Exception):
+    def __init__(self):
+        pass
 
 
-class Deque:
+class MaxItemsException(Exception):
+    def __init__(self):
+        pass
+
+
+class Deck:
     def __init__(self, max_size):
-        self.__items = [None] * max_size
-        self.__max_size = max_size
-        self.__head = 0
-        self.__tail = 0
-        self.__size = 0
+        self._array = [None] * max_size
+        self._max_size = max_size
+        self._size = 0
+        self._head = 0
+        self._tail = -1
 
-    def __check_full_queue(self):
-        return self.__size == self.__max_size
+    @property
+    def is_full(self):
+        return self._size == self._max_size
 
-    def __check_empty_queue(self):
-        return self.__size == 0
-
-    def __get_index(self, method):
-        if method == 'push_front':
-            if self.__check_empty_queue():
-                self.__head = 0
-                self.__tail = 0
-                return 0
-            return (self.__head - 1) % self.__max_size
-        if method == 'push_back':
-            if self.__check_empty_queue():
-                self.__head = 0
-                self.__tail = 0
-                return 0
-            return (self.__tail + 1) % self.__max_size
-        if method == 'pop_front':
-            return (self.__head + 1) % self.__max_size
-        if method == 'pop_back':
-            return (self.__tail - 1) % self.__max_size
-
-    def push_front(self, value):
-        """Метод push_front добавляет элемент в начало дека"""
-        if self.__check_full_queue():
-            raise RuntimeError(ERROR_MESSAGE)
-        self.__head = self.__get_index('push_front')
-        self.__items[self.__head] = value
-        self.__size += 1
+    @property
+    def is_empty(self):
+        return self._size == 0
 
     def push_back(self, value):
-        """Метод push_back добавляет элемент в конец дека"""
-        if self.__check_full_queue():
-            raise RuntimeError(ERROR_MESSAGE)
-        self.__tail = self.__get_index('push_back')
-        self.__items[self.__tail] = value
-        self.__size += 1
+        if self.is_full:
+            raise MaxItemsException
+        else:
+            self._tail = (self._tail + 1) % self._max_size
+            self._array[self._tail] = value
+            self._size += 1
 
-    def pop_front(self):
-        """Метод pop_front удаляет элемент из начала дека и возвращает его"""
-        if self.__check_empty_queue():
-            raise RuntimeError(ERROR_MESSAGE)
-        value = self.__items[self.__head]
-        self.__head = self.__get_index('pop_front')
-        self.__size -= 1
-        return value
+    def push_front(self, value):
+        if self.is_full:
+            raise MaxItemsException
+        else:
+            self._head = (self._head - 1) % self._max_size
+            self._array[self._head] = value
+            self._size += 1
 
     def pop_back(self):
-        """Метод pop_back удаляет элемент из конца дека и возвращает его"""
-        if self.__check_empty_queue():
-            raise RuntimeError(ERROR_MESSAGE)
-        value = self.__items[self.__tail]
-        self.__tail = self.__get_index('pop_back')
-        self.__size -= 1
-        return value
+        if self.is_empty:
+            raise NoItemsException
+        else:
+            value = self._array[self._tail]
+            self._tail = (self._tail - 1) % self._max_size
+            self._size -= 1
+            return value
+
+    def pop_front(self):
+        if self.is_empty:
+            raise NoItemsException
+        else:
+            value = self._array[self._head]
+            self._head = (self._head + 1) % self._max_size
+            self._size -= 1
+            return value
 
 
-if __name__ == '__main__':
-    command_count = int(input())
-    deque = Deque(max_size=int(input()))
-    for _ in range(command_count):
-        try:
-            command, *params = input().strip().split(' ')
-            result = getattr(deque, command)(*params)
-            if 'pop' in command:
-                print(result)
-        except RuntimeError:
-            print(ERROR_MESSAGE)
-        except AttributeError:
-            raise ValueError(ERROR_COMMAND.format(command=command))
+n = int(input())
+m = int(input())
+
+deck = Deck(m)
+
+for _ in range(n):
+    try:
+        cmd = input().split(' ')
+        if len(cmd) == 1:
+            print(getattr(deck, cmd[0])())
+        else:
+            getattr(deck, cmd[0])(cmd[1])
+    except NoItemsException:
+        print('error')
+    except MaxItemsException:
+        print('error')
